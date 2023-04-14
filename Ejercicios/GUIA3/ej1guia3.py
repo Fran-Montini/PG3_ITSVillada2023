fc6e97b11a53d429c0d9989eeb2e942a98bf8888
fran-monti123 <francismontini@gmail.com>
2023-04-14 11:42:17 -0300

    ej1guia3

---
+++ Ejercicios/GUIA3/ej1guia3.py
@@ -0,0 +1,11 @@
+while True:
+    try:
+        num1 = int(input("Ingrese el primer numero entero: "))
+        num2 = int(input("Ingrese el segundo numero entero: "))
+        suma = num1 + num2
+        print("La suma es:", suma)
+        opcion = input("¿Desea seguir sumando valores? (s/n): ")
+        if opcion.lower() != 's':
+            break
+    except ValueError:
+        print("Error: Debe ingresar un numero  entero.")
